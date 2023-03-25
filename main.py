69c48adfd33671d41cd3bcf87c77d8e73546d79f
Mouad scriptz <102869034+Mouad-scriptz@users.noreply.github.com>
2023-03-25 15:27:49 +0000

    10x Faster + Proxy support

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import requests, jwt, hashlib, math, time, random, os, threading
+import jwt, hashlib, math, time, random, os, threading, tls_client
 from python_ghost_cursor import path
 from datetime import datetime
 from json import dumps
 if not "images" in os.listdir(os.curdir):
     os.mkdir("images")
+if open("proxies.txt").read() == "<# Free proxies work. #>":
+    print("(-) No proxies detected in proxies.txt")
+    input("(#) Press ENTER to continue.")
 def generate_hsl(req):
     x = "0123456789/:abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     req = jwt.decode(req,options={"verify_signature":False})
@@ -56,38 +59,45 @@ def generate_hsl(req):
         result
     ])
     return hsl
-
+v = tls_client.Session(client_identifier="chrome_111").get("https://js.hcaptcha.com/1/api.js").text.split('nt="')[1].split('"')[0]
 class Scrapper():
     def __init__(self, site_key: str, host: str):
         self.sk = site_key
         self.host = host 
-        self.v = requests.get("https://js.hcaptcha.com/1/api.js").text.split('nt="')[1].split('"')[0]
+        self.session = tls_client.Session(client_identifier="chrome_111",random_tls_extension_order=True)
+        self.session.headers={
+            'authority': 'hcaptcha.com',
+            'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+        }
+        with open("proxies.txt","r") as f:
+            proxy = random.choice(f.read().splitlines())
+            f.close()
+        self.session.proxies={
+            "http": "http://"+proxy,
+            "https": "http://"+proxy,
+        }
     def get_c(self):
         headers = {
-            'authority': 'hcaptcha.com',
             'accept': 'application/json',
             'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,ar;q=0.7',
             'cache-control': 'no-cache',
             'content-type': 'text/plain',
             'origin': 'https://newassets.hcaptcha.com',
             'pragma': 'no-cache',
-            'referer': 'https://newassets.hcaptcha.com/',
-            'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
             'sec-fetch-dest': 'empty',
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
         }
-        params = {'v':self.v,'host':self.host,'sitekey':self.sk,'sc':'1','swa':'1'}
-        r = requests.post('https://hcaptcha.com/checksiteconfig',params=params,headers=headers)
+        params = {'v':v,'host':self.host,'sitekey':self.sk,'sc':'1','swa':'1'}
+        r = self.session.post('https://hcaptcha.com/checksiteconfig',params=params,headers=headers)
         c = r.json()["c"]
         c["type"] = "hsl"
         return c
     def get_captcha(self,c,hsl):
         headers = {
-            'authority': 'hcaptcha.com',
             'accept': 'application/json',
             'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,ar;q=0.7',
             'cache-control': 'no-cache',
@@ -95,20 +105,18 @@ class Scrapper():
             'origin': 'https://newassets.hcaptcha.com',
             'pragma': 'no-cache',
             'referer': 'https://newassets.hcaptcha.com/',
-            'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
             'sec-fetch-dest': 'empty',
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
         }
         start = {'x': 100, 'y': 100}
         end = {'x': 600, 'y': 700}
         timestamp = int((time.time() * 1000) + round(random.random() * (120 - 30) + 30))
         mm = [[int(p['x']), int(p['y']), int(time.time() * 1000) + round(random.random() * (5000 - 2000) + 2000)] for p in path(start, end)]
         payload = {
-            "v": self.v,
+            "v": v,
             "sitekey": self.sk,
             "host": self.host,
             "hl": "en",
@@ -116,7 +124,7 @@ class Scrapper():
             "n": hsl,
             "c": dumps(c,separators=(",",":"))
         }
-        r = requests.post(f'https://hcaptcha.com/getcaptcha/{self.sk}',headers=headers,data=payload)
+        r = self.session.post(f'https://hcaptcha.com/getcaptcha/{self.sk}',headers=headers,data=payload)
         return r.json()
     def scrape_challenges(self):
         while True:
@@ -129,7 +137,7 @@ class Scrapper():
                     os.mkdir(f"images/{folder_name}")
                 for i in captcha_data["tasklist"]:
                     image_name = i["task_key"]
-                    image = requests.get(i["datapoint_uri"])
+                    image = self.session.get(i["datapoint_uri"])
                     f = open(f"images/{folder_name}/{image_name}.png","wb")
                     f.write(image.content)
                     f.close()
@@ -137,9 +145,9 @@ class Scrapper():
                 if not question in f.read().splitlines():
                     f.write(question+"\n")
                 f.close()
-                print(f"Question: {question} | Images Scrapped: {len(captcha_data['tasklist'])}")
+                print(f"(+) Question: {question} | Images Scrapped: {len(captcha_data['tasklist'])} | {len(os.listdir(f'images/{folder_name}'))}")
             except:
                 pass
-threads = range(int(input("Threads >> ")))
+threads = range(int(input("(>) Threads >> ")))
 for i in threads:
     threading.Thread(target=Scrapper("4c672d35-0701-42b2-88c3-78380b0db560","discord.com").scrape_challenges).start()
